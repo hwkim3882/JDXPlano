38810e12372ab286a0a5c1cbb1eccd26263d5427
hwkim3882 <kimhyowon1@gmail.com>
2025-07-29 12:42:52 -0500

    Fix aws-exports import path with .js extension

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css'; // Tailwind CSS import
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.js';
 
 Amplify.configure(awsExports);
 
